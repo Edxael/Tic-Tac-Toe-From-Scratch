f7f30f68e270d288615307d90bf156cfdf6da365
Edmundo rubio <edmundo@webaholics.co>
2019-02-20 17:41:43 -0700

    add new files

---
+++ src/TicApp/Pages/00-Main.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Game from './01-Game'
+
+class Main extends Component{
+  render(){
+    return(
+      <div>
+        <Game />
+      </div>
+    )
+  }
+}
+
+export default Main
